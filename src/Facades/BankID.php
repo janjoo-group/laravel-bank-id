96154079dee77f77c0a1113aa0be72562df7a452
Johannes Eriksson <johannes.eriksson@hyperisland.se>
2024-01-26 13:22:30 +0100

    style: correct type hints to facade docblock

--- src/Facades/BankID.php
+++ src/Facades/BankID.php
@@ -5,11 +5,11 @@ namespace Jgroup\BankID\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Jgroup\BankID\Service\Models\Http\TransactionResponse auth(string $endUserIp, string $userVisibleData = null, string $userVisibleDataFormat = null, string $userNonVisibleData = null, array $requirement = null)
- * @method static \Jgroup\BankID\Service\Models\Http\TransactionResponse sign(string $endUserIp, string $userVisibleData, string $userVisibleDataFormat = null, string $userNonVisibleData = null, array $requirement = null)
- * @method static \Jgroup\BankID\Service\Models\Http\CollectResponse collect()
+ * @method static \Jgroup\BankID\Service\Models\Http\TransactionResponse|null auth(string $endUserIp, string $userVisibleData = null, string $userVisibleDataFormat = null, string $userNonVisibleData = null, array $requirement = null)
+ * @method static \Jgroup\BankID\Service\Models\Http\TransactionResponse|null sign(string $endUserIp, string $userVisibleData, string $userVisibleDataFormat = null, string $userNonVisibleData = null, array $requirement = null)
+ * @method static \Jgroup\BankID\Service\Models\Http\CollectResponse|null collect()
  * @method static \Jgroup\BankID\Service\Models\Http\CancelResponse cancel()
- * @method static \Jgroup\BankID\Service\Models\BankIDTransaction getSessionTransaction()
+ * @method static \Jgroup\BankID\Service\Models\BankIDTransaction|null getSessionTransaction()
  * @method static void setSessionTransaction(\Jgroup\BankID\Service\Models\BankIDTransaction $transaction)
  *
  * @see \Jgroup\BankID\Service\BankIDService
